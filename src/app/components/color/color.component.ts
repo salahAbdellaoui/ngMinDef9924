1d0affb3824910f743241f214112bd2253857651
FIRST_NAME LAST_NAME <FIRST_NAME@LAST_NAME.com>
2024-09-09 13:17:01 +0100

    ll

--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-color',
   templateUrl: './color.component.html',
-  styleUrls: ['./color.component.css']
+  styleUrls: ['./color.component.css'],
 })
 export class ColorComponent {
   color = 'red';
+  defaultColor() {
+    this.color = 'red';
+  }
 }
