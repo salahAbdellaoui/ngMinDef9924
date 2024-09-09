7c93c34d9924ab520bbf568600165b000b59e3af
FIRST_NAME LAST_NAME <FIRST_NAME@LAST_NAME.com>
2024-09-09 13:27:06 +0100

    tt0

--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -6,23 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./color.component.css'],
 })
 export class ColorComponent {
-<<<<<<< HEAD
   color = 'red';
   defaultColor() {
     this.color = 'red';
-=======
-  // State
-  defaultColor = 'red';
-  color = this.defaultColor;
-
-  // coùportements
-  changeColor(newColorInput: HTMLInputElement): void {
-    this.color = newColorInput.value;
-    newColorInput.value = '';
-  }
-
-  reset(): void {
-    this.color = this.defaultColor;
->>>>>>> 5b15afe26826febf97fb7fb80b116ca2490843e4
   }
 }
